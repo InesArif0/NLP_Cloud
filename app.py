2e20744af5db807977cce86f2f62fa610219bac8
InesArif0 <116158792+InesArif0@users.noreply.github.com>
2022-11-12 13:47:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from predictions import predict
 st.title("Topics Analyze")
 text = st.text_input('Critique à analyser')
 
-nbrTopics = st.number_input('Nombre de topics')
+nbrTopics = int(st.number_input('Nombre de topics'))
 bool_button = st.button('Détecter le topic')
 
 if bool_button == True :
-    polarity, topics = predict(text, int(nbrTopics))
+    polarity, topics = predict(text, nbrTopics)
     if polarity > 0:
         st.warning("Polarité: " + str(polarity) + " (COMMENTAIRE POSITIF)")
     else :
